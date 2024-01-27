07bc3b4e106d7f775e2ecbe56457df20ba3be66e
Fausto Mastrodomenico <faustomastro96@gmail.com>
2024-01-27 12:21:30 -0500

    Se realizó una reestructuración del código para mejorar la legibilidad del mísmo

--- game.js
+++ game.js
@@ -12,24 +12,26 @@ function startGame (){
     game.font = elementsSize + 'px Verdana'
     game.textAlign = 'center';
 
-    const map = maps[1];
+    const map = maps[0];
     const mapRows = map.trim().split('\n');
     const mapMatrix = mapRows.map(row => row.trim().split(''))
     console.log({map,mapRows,mapMatrix});
 
-    for (let x = 1; x <= 10; x++) {
-        for (let y = 1; y <= 10 ; y++)
-        game.fillText(emojis[mapMatrix[x - 1][y - 1]] , elementsSize * y , elementsSize * x);
-    }
+    mapMatrix.forEach((row, rowIndex) => {
+        row.forEach((col, colIndex) => {
+            const emoji = emojis[col]
+            const posX  = elementsSize * (colIndex + 1);
+            const posY = elementsSize * (rowIndex + 1);
+            game.fillText(emoji, posX, posY)
+        });
+    });
+
+    // for (let x = 1; x <= 10; x++) {
+    //     for (let y = 1; y <= 10 ; y++)
+    //     game.fillText(emojis[mapMatrix[x - 1][y - 1]] , elementsSize * y , elementsSize * x);
+    // }
     
-    // game.fillRect(0,0,100,100);
-    // game.clearRect(0,0,100,50);
-    // game.clearRect(50,50,50,50);
-    // game.clearRect(0,0,50,50);
-    // game.font = '25px Verdana'
-    // game.fillStyle = 'purple'
-    // game.textAlign = 'left'
-    // game.fillText('Juego',100,100)
+
 }
 
 function setCanvasSize () {
